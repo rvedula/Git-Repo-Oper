d9403dcc50d125b960e862046218f8059725863a
rvedula <rvedula@oceaneering.com>
2020-06-18 09:50:38 -0500

    Create environment.ts

---
+++ environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  notificationEngineUrl : 'http://localhost:5009',
+  msalConfig: {
+    clientId: '28c62653-6ad9-4ed7-bcbd-2752f1152f45',
+    tenantId: '97525e9a-595d-472c-8248-0dc58f852d61',
+    redirectUri: 'https://ursadev.oii.oceaneering.com/login',
+    postLogoutRedirectUri: 'https://ursadev.oii.oceaneering.com/login',
+  }
+};
